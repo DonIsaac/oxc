3ce971551078de2a69845e85699d637a9dd1accf
Don Isaac <donald.isaac@gmail.com>
2024-10-04 19:26:20 -0400

    add diagnostics list in Checker

--- crates/oxc_type_checker/src/checker/mod.rs
+++ crates/oxc_type_checker/src/checker/mod.rs
@@ -4,10 +4,14 @@ mod type_factory;
 mod type_inquisition;
 mod type_instantiation;
 
-use std::{cell::Ref, rc::Rc};
+use std::{
+    cell::{Ref, RefCell},
+    rc::Rc,
+};
 
 use oxc_allocator::Allocator;
 use oxc_cfg::ControlFlowGraph;
+use oxc_diagnostics::OxcDiagnostic;
 use oxc_semantic::Semantic;
 use oxc_syntax::types::{TypeFlags, TypeId};
 
@@ -20,12 +24,20 @@ use crate::{
 /// ## References
 /// - <https://gist.github.com/Boshen/d189de0fe0720a30c5182cb666e3e9a5>
 pub struct Checker<'a> {
+    /// `compilerOptions` in `tsconfig.json`
     settings: CheckerSettings,
+    /// Store and create new types
     builder: TypeBuilder<'a>,
+    /// built-in intrinsic type (any, boolean, number, etc).
     intrinsics: Intrinsics,
+    /// Semantic analysis results
     semantic: Rc<Semantic<'a>>,
+    /// Cache of already-created types
     cache: TypeCache<'a>,
+    /// Node/symbol links
     links: Links<'a>,
+    /// Errors discovered while checking
+    diagnostics: RefCell<Vec<OxcDiagnostic>>,
 }
 
 // public interface
@@ -44,8 +56,9 @@ impl<'a> Checker<'a> {
         let cache = TypeCache::new(alloc);
         let intrinsics = Intrinsics::new(&builder, &settings, &cache);
         let links = Links::default();
+        let diagnostics = RefCell::new(vec![]);
 
-        Self { settings, builder, intrinsics, semantic, cache, links }
+        Self { settings, builder, intrinsics, semantic, cache, links, diagnostics }
     }
 }
 
@@ -63,5 +76,10 @@ impl<'a> Checker<'a> {
     pub(self) fn cfg(&self) -> &ControlFlowGraph {
         // SAFETY: we assert a CFG exists when Checker is created.
         unsafe { self.semantic.cfg().unwrap_unchecked() }
+    }
+
+    /// Report a check error.
+    pub(crate) fn error(&self, diagnostic: OxcDiagnostic) {
+        self.diagnostics.borrow_mut().push(diagnostic)
     }
 }
